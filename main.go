6e80eebdb9d48c8fe6e00b354e54021516e53594
Doosan Beak <emptyfridge0900@gmail.com>
2025-08-09 09:54:02 +0900

    Testing seding and receiving message to clients. works fine

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
-package greetings
+package main
 
+// the code below is from https://medium.com/@parvjn616/building-a-websocket-chat-application-in-go-388fff758575
 import (
 	"fmt"
 	"net/http"
@@ -8,8 +9,26 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Run the chat server")
+
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	fmt.Println("Server started on : 4545")
+	err := http.ListenAndServe(":4545", nil)
+	if err != nil {
+		panic("Error starting server: " + err.Error())
+	}
+
+}
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Chat Room!")
 }
 
 var upgrader = websocket.Upgrader{
@@ -27,31 +46,56 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer conn.Close()
+
+	clients[conn] = true
+
 	for {
 		var msg Message
 		err := conn.ReadJSON(&msg)
 		if err != nil {
+			fmt.Println(err)
+			delete(clients, conn)
 			return
 		}
 
-		if msg.Type == "new_client" {
-			//add new user
-		} else if msg.Type == "chat" {
-			//chat broadcast
-		} else if msg.Type == "session_end" {
-			//remove client
-		} else {
-			//log.Println("Unknown message:", msg.Type)
+		broadcast <- msg
+	}
+
+	// for {
+	// 	var msg Message
+	// 	err := conn.ReadJSON(&msg)
+	// 	if err != nil {
+	// 		return
+	// 	}
+
+	// 	if msg.Type == "new_client" {
+	// 		//add new user
+	// 	} else if msg.Type == "chat" {
+	// 		//chat broadcast
+	// 	} else if msg.Type == "session_end" {
+	// 		//remove client
+	// 	} else {
+	// 		//log.Println("Unknown message:", msg.Type)
+	// 	}
+	// }
+}
+
+func handleMessages() {
+	for {
+		//got message from channel
+		msg := <-broadcast
+		fmt.Println(msg.Text)
+
+		//loop through the client list and sending the message to the client
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				fmt.Println(err)
+				client.Close()
+				delete(clients, client)
+			}
 		}
 	}
-}
-
-func sendMessage(c *websocket.Conn, clientID string) {
-
-}
-
-func addNewUser() {
-
 }
 
 type Message struct {
