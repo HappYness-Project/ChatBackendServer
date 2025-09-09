d2cc86b8751a2848314ef6185e7d7891b0143231
kevin <hyunbin7303@gmail.com>
2025-09-08 22:58:22 -0400

    Mistype - Delete participant from chat route should be Delete verb

--- internal/chat/route/routes.go
+++ internal/chat/route/routes.go
@@ -36,7 +36,7 @@ func (h *Handler) RegisterRoutes(router chi.Router) {
 	router.Delete("/api/user-groups/{groupID}/chat", h.RemoveChatByUserGroupId)
 	router.Get("/api/chats/{chatID}/chat-participants", h.GetChatParticipants)
 	router.Post("/api/chats/{chatID}/chat-participants", h.AddChatParticipant)
-	router.Post("/api/chats/{chatID}/chat-participants/{participantID}", h.DeleteParticipantFromChat)
+	router.Delete("/api/chats/{chatID}/chat-participants/{participantID}", h.DeleteParticipantFromChat)
 }
 func (h *Handler) GetChatById(w http.ResponseWriter, r *http.Request) {
 	chatID := chi.URLParam(r, "chatID")
